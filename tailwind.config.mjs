904ffdbe5f1b87ee0340f59403603798bbab8c29
Husban Baig <saifanees11@gmail.com>
2025-02-19 20:54:47 +0500

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,13 +11,6 @@ export default {
         "yellow-tail": ["Yellowtail-Regular", "sans-serif"],
         roboto: ["Roboto-Regular", "sans-serif"],
         robotoMedium: ["Roboto-Medium", "sans-serif"],
-        clash: ["ClashGrotesk-Variable", "sans-serif"],
-        clashExtralight: ["ClashGrotesk-Extralight", "sans-serif"],
-        clashLight: ["ClashGrotesk-Light", "sans-serif"],
-        clashRegular: ["ClashGrotesk-Regular", "sans-serif"],
-        clashMedium: ["ClashGrotesk-Medium", "sans-serif"],
-        clashSemibold: ["ClashGrotesk-Semibold", "sans-serif"],
-        clashBold: ["ClashGrotesk-Bold", "sans-serif"],
       },
       colors: {
         background: "var(--background)",
